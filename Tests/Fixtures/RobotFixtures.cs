c2a6c3d8196062c6dc43d26e4a966025701722fe
Vinicius De Martin <demartinvinicius@gmail.com>
2023-03-01 10:23:27 -0300

    Test strucure initialized

---
+++ Tests/Fixtures/RobotFixtures.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TheRobot;
+
+namespace RobotTests.Fixtures;
+
+public class RobotFixtures : IDisposable
+{
+    public readonly Robot Robot;
+    public readonly IHttpClientFactory HttpCientFactory;
+
+    public RobotFixtures()
+    {
+        IHost host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddHttpClient();
+            }).Build();
+        HttpCientFactory = host.Services.GetRequiredService<IHttpClientFactory>();
+        Robot = new Robot(HttpCientFactory);
+    }
+
+    public void Dispose()
+    {
+        Robot.Dispose();
+    }
+}
--- Tests/SimpleRobotTests.cs
+++ Tests/SimpleRobotTests.cs
@@ -1,10 +1,19 @@
+using RobotTests.Fixtures;
+
 namespace RobotTests;
 
-public class SimpleRobotTests
+public class SimpleRobotTests : IClassFixture<RobotFixtures>
 {
+    private RobotFixtures robotFixtures;
+
+    public SimpleRobotTests(RobotFixtures robotFixtures)
+    {
+        this.robotFixtures = robotFixtures;
+    }
+
     [Fact]
     public void Test1()
     {
-
+        Assert.NotNull(robotFixtures);
     }
 }
